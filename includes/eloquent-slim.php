9b9af8627b676771ad3317774511911d6374a377
Trent Hogan <trenthogan1@gmail.com>
2014-11-17 23:34:50 +1300

    first commit

---
+++ includes/eloquent-slim.php
@@ -0,0 +1,23 @@
+<?php
+
+/** 
+* Bootstrap Laravel ORM
+*
+*/
+
+use Illuminate\Database\Capsule\Manager as Capsule;  
+
+$capsule = new Capsule; 
+
+$capsule->addConnection(array(
+    'driver'    => 'mysql',
+    'host'      => 'localhost',
+    'database'  => 'slim',
+    'username'  => 'root',
+    'password'  => 'root',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => ''
+));
+
+$capsule->bootEloquent();
---
+++ models/users.php
@@ -0,0 +1,8 @@
+<?php
+/** 
+* Users Model
+*/
+class Users extends Illuminate\Database\Eloquent\Model {
+	//Turn off laravel time stamps..
+    public $timestamps = false;
+}
